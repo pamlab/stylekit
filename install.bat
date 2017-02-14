c4b0627f2106f18633a827e8aa3754fc20f5b14b
Hirofumi Iji <h_iji@pam.co.jp>
2017-02-14 14:04:37 +0900

    Update: modules / README.md

--- install.bat
+++ install.bat
@@ -2,4 +2,4 @@
 
 cd /d %~dp0
 
-npm install -g postcss-cli autoprefixer postcss-flexbugs-fixes postcss-custom-properties postcss-nested
+npm install -g postcss-cli autoprefixer postcss-flexbugs-fixes postcss-custom-properties postcss-nesting postcss-custom-media postcss-pseudo-class-any-link postcss-apply
