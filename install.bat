023eea97daa3dd547cdb1b166ca81dc5fc051d31
Hirofumi Iji <h_iji@pam.co.jp>
2017-02-14 14:07:46 +0900

    Add: images

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd /d %~dp0
-
-npm install -g postcss-cli autoprefixer postcss-flexbugs-fixes postcss-custom-properties postcss-nesting postcss-custom-media postcss-pseudo-class-any-link postcss-apply
